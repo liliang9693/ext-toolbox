287355b5976c53129c5b74ffebe6e3e601934d51
liliang9693 <liliang_wk@163.com>
2020-05-26 16:02:33 +0800

    0.0.1 test

---
+++ arduinoC/main.ts
@@ -0,0 +1,41 @@
+
+enum SIZE {
+    //% block="29*29"
+    1,
+    //% block="58*58"
+    2
+}
+
+enum LINE {
+    //% block="1"
+    1,
+    //% block="2"
+    2,
+    //% block="3"
+    3,
+    //% block="4"
+    4
+}
+
+enum BTN {
+    //% block="A"
+    A,
+    //% block="B"
+    B,
+    //% block="A+B"
+    AB
+}
+
+
+//% color="#1ca2e9" iconWidth=50 iconHeight=40
+namespace toolbox {
+    //% block="取整 [FLOAT]" blockType="reporter"
+    //% FLOAT.shadow="number" 
+    export function changeInt(parameter: any, block: any) {
+        let flt=parameter.FLOAT.code;
+        Generator.addCode([`int(${flt})`,Generator.ORDER_UNARY_POSTFIX]);
+
+    }
+                
+
+}
